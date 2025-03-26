7d6d8a3bb69d237d7f4edd27f98b98f30c6f10f9
Jsson47 <martinezvalle98@gmail.com>
2025-03-26 13:48:08 -0600

    style: adjust logo positioning and styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import BannerLogo from './img/banner-custom.png';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="banner-logo-container">
+        <img 
+          src={BannerLogo}
+          className='banner-logo'
+          alt='Banner del Princio de la Aplicación' />
+      </div>
     </div>
   );
 }
